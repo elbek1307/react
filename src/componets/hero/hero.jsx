6f88160ec0ccd87b3a857e14989104b42ac0d85c
elbek1307 <elbekjonabdushukurov1307@gmail.com>
2026-04-29 15:47:52 +0500

    hero finished

--- src/componets/hero/hero.jsx
+++ src/componets/hero/hero.jsx
@@ -1,6 +1,7 @@
 
-
 import "./hero.css"
+
+import hero_img from "../hero/heroimg.png"
 
 function Hero(){
     return(
@@ -8,10 +9,11 @@ function Hero(){
          <section className="hero">
             <div className="container">
                 <div className="hero_content">
-                    <hr />
                     <h1 className="hero_title">Humanizing <br />your insurance.</h1>
+                    <p className="hero_text">Get your life insurance coverage easier and faster. We blend our expertise and technology to help you find the plan that’s right for you. Ensure you and your loved ones are protected.</p>
+                    <button className="hero_btn">VIEW PLANS</button>
                 </div>
-                <img src="" alt="" className="hero_img" />
+                <img src={hero_img} alt="" className="hero_img" />
             </div>
          </section>
         </>
