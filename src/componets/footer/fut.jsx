724d7a987fa83b4d7d2f5d052d896789540363f9
elbek1307 <elbekjonabdushukurov1307@gmail.com>
2026-05-01 00:01:22 +0500

    footer bitdi

--- src/componets/footer/fut.jsx
+++ src/componets/footer/fut.jsx
@@ -20,7 +20,29 @@ function Footer() {
           </div>
         </div>
         <hr className="fut_hr" />
-        <div className="fut_bottom"></div>
+        <div className="fut_bottom">
+            <div className="fut_list">
+                <h3 className="fut_bot_title">OUR COMPANY</h3>
+                <a href="" className="fut_bot_link">HOW WE WORK</a>
+                <a href="" className="fut_bot_link">WHY INSURE?</a>
+            </div>
+            <div className="fut_list">
+                <h3 className="fut_bot_title">HELP ME</h3>
+                <a href="" className="fut_bot_link">FAQ</a>
+                <a href="" className="fut_bot_link">TERMS OF USE</a>
+            </div>
+            <div className="fut_list">
+                <h3 className="fut_bot_title">CONTACT</h3>
+                <a href="" className="fut_bot_link">SALES</a>
+                <a href="" className="fut_bot_link">SUPPORT</a>
+            </div>
+            <div className="fut_list">
+                <h3 className="fut_bot_title">OTHERS</h3>
+                <a href="" className="fut_bot_link">CAREERS</a>
+                <a href="" className="fut_bot_link">PRESS</a>
+            </div>
+    
+        </div>
       </div>
     </footer>
   );
