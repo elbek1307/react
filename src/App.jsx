387eed471b995f537c4b534a22a6ade2cc54dce5
elbek1307 <elbekjonabdushukurov1307@gmail.com>
2026-05-04 00:38:32 +0500

    prop ishlatildi

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,27 @@ import Footer from "./componets/footer/fut.jsx"
 import img1 from "./componets/different/dif1.svg";
 import img2 from "./componets/different/dif2.svg";
 import img3 from "./componets/different/dif3.svg";
+
+
+
 const datas = [
   {
-    id: 1,  url: dif1, title: "Snappy Process", text: "Our application process can be completed in minutes, not hours. Don’t get stuck filling in tedious forms."
+    id: 1,
+    url: img1,
+    title: "Snappy Process",
+    text: "Our application process can be completed in minutes, not hours. Don’t get stuck filling in tedious forms."
+  },
+  {
+    id: 2,
+    url: img2,
+    title: "Affordable Prices",
+    text: "We don’t want you worrying about high monthly costs. Our prices may be low, but we still offer the best coverage possible."
+  },
+  {
+    id: 3,
+    url: img3,
+    title: "People First",
+    text: "Our plans aren’t full of conditions and clauses to prevent payouts. We make sure you’re covered when you need."
   }
 ];
 
--- src/componets/different/dif.jsx
+++ src/componets/different/dif.jsx
@@ -1,43 +1,23 @@
+import "./dif.css";
 
-import dif1 from "../different/dif2.svg"
-
-
-import "./dif.css"
-
-function Dif ({data}){
-    return <>
-     <section className="dif">
-        <div className="container">
-            <hr className="dif_hr" />
-            <h2 className="dif_title">We’re different</h2>
-            <div className="dif_list">
-
-
-              {data.map((item)=>(
-                  <div key={item.id} className="dif_item">
-                    <img src={item.url} alt="" className="dif_itm_img" />
-                    <h3 className="dif_item_title">{item.title}</h3>
-                    <p className="dif_item_text">{item.text}</p>
-                </div> 
-              ))}
-
-
-               
-                {/* <div className="dif_item">
-                    <img src={dif2} alt="" className="dif_itm_img" />
-                    <h3 className="dif_item_title">Affordable Prices</h3>
-                    <p className="dif_item_text">We don’t want you worrying about high monthly costs. Our prices may be low, but we still offer the best coverage possible.</p>
-                </div> */}
-                {/* <div className="dif_item">
-                    <img src={dif3} alt="" className="dif_itm_img" />
-                    <h3 className="dif_item_title">People First</h3>
-                    <p className="dif_item_text">Our plans aren’t full of conditions and clauses to prevent payouts. We make sure you’re covered when you need </p>
-                </div> */}
+function Dif({ data }) {
+  return (
+    <section className="dif">
+      <div className="container">
+        <hr className="dif_hr" />
+        <h2 className="dif_title">We’re different</h2>
+        <div className="dif_list">
+          {data && data.map((item) => (
+            <div key={item.id} className="dif_item">
+              <img src={item.url} alt={item.title} className="dif_itm_img" />
+              <h3 className="dif_item_title">{item.title}</h3>
+              <p className="dif_item_text">{item.text}</p>
             </div>
+          ))}
         </div>
-     </section>
-
-    </>
+      </div>
+    </section>
+  );
 }
 
-export default Dif
+export default Dif;
