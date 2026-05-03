7fb4f5b756d97182c2fa137b81eecaeeb7a38791
elbek1307 <elbekjonabdushukurov1307@gmail.com>
2026-05-04 00:43:05 +0500

    yana ozgartish kiritildi

--- src/componets/different/dif.jsx
+++ src/componets/different/dif.jsx
@@ -1,23 +1,31 @@
-import "./dif.css";
 
-function Dif({ data }) {
-  return (
-    <section className="dif">
-      <div className="container">
-        <hr className="dif_hr" />
-        <h2 className="dif_title">We’re different</h2>
-        <div className="dif_list">
-          {data && data.map((item) => (
-            <div key={item.id} className="dif_item">
-              <img src={item.url} alt={item.title} className="dif_itm_img" />
-              <h3 className="dif_item_title">{item.title}</h3>
-              <p className="dif_item_text">{item.text}</p>
+import dif1 from "../different/dif2.svg"
+
+
+import "./dif.css"
+
+function Dif ({data}){
+    return <>
+     <section className="dif">
+        <div className="container">
+            <hr className="dif_hr" />
+            <h2 className="dif_title">We’re different</h2>
+            <div className="dif_list">
+
+
+              {data && data.map((item)=>(
+                  <div key={item.id} className="dif_item">
+                    <img src={item.url} alt="" className="dif_itm_img" />
+                    <h3 className="dif_item_title">{item.title}</h3>
+                    <p className="dif_item_text">{item.text}</p>
+                </div> 
+              ))}
+
             </div>
-          ))}
         </div>
-      </div>
-    </section>
-  );
+     </section>
+
+    </>
 }
 
-export default Dif;
+export default Dif
