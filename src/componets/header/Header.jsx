326c85c82ed148b072a3ca054b08e2f1a089ae48
elbek1307 <elbekjonabdushukurov1307@gmail.com>
2026-05-11 05:22:45 +0500

    form css tugadi

--- src/componets/header/Header.jsx
+++ src/componets/header/Header.jsx
@@ -1,42 +1,60 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 import { IoMdExit } from "react-icons/io";
 
-import logo from "../header/INSURE.svg"
-import "./header.css"
+import logo from "../header/INSURE.svg";
+import "./header.css";
 
-function Header(){
-
-   const [modal, setmodal] = useState(false)
-
+function Header() {
+  const [modal, setmodal] = useState(false);
 
   return (
     <>
-    <nav>
-      <div className="container">
-       <a href="#"> <img src={logo} alt="sie logo" className="nav_logo" /></a>
+      <nav>
+        <div className="container">
+          <a href="#">
+            {" "}
+            <img src={logo} alt="sie logo" className="nav_logo" />
+          </a>
 
-        <div className="nav_box">
-          <div className="nav_list">
-           <a href="#" className="nav_item">HOW WE WORK</a>
-           <a href="#" className="nav_item">BLOG</a>
-           <a href="#" className="nav_item" onClick={()=> setmodal(!modal)}>ACCOUNT</a>
-            
-          </div>
+          <div className="nav_box">
+            <div className="nav_list">
+              <a href="#" className="nav_item">
+                HOW WE WORK
+              </a>
+              <a href="#" className="nav_item">
+                BLOG
+              </a>
+              <a href="#" className="nav_item" onClick={() => setmodal(!modal)}>
+                ACCOUNT
+              </a>
+            </div>
 
-          <button className="nav_btn">VIEW PLANS</button>
-
+            <button className="nav_btn">VIEW PLANS</button>
 
             {modal && (
               <div className="mod_back">
-              < IoMdExit className='mod_exit' onClick={()=> setmodal(false)}/>
-            </div>
-          )}
-        </div>
-        
-      </div>
-    </nav>
-    
-    </>)}
+                <IoMdExit
+                  className="mod_exit"
+                  onClick={() => setmodal(false)}
+                />
 
-export default Header
+                <form className="form">
+                  <label htmlFor="name">Login</label>
+                  <input type="text" id="name" placeholder="Login" />
+
+                  <label htmlFor="parol">Password</label>
+                  <input type="password" id="parol" placeholder="Password" />
+
+                  <button type="submit" className="mod_btn">Send</button>
+                </form>
+              </div>
+            )}
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default Header;
